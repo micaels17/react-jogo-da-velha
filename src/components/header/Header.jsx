86e2a135d62a24a2657486db6b0c656b213bfffc
Micael Silva <micaelss@gmail.com>
2024-04-20 18:00:15 -0300

    Inicializando o projeto

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import styles from './Header.module.css'
+
+import Title from "../title/Title"
+import Subtitle from "../subtitle/Subtitle"
+
+
+function Header () {
+  return (
+    <div className={styles.header}>
+       <Title>Jogo da Velha</Title>
+      <Subtitle>Criado por Micael Silva</Subtitle>
+    </div>
+  )
+}
+
+export default Header
