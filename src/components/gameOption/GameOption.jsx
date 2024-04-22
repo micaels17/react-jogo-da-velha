ecc969589537f77c3e3de78d58fa3c5acc4c98f8
Micael Silva <micaelss@gmail.com>
2024-04-22 10:45:25 -0300

    Criado componente Game

---
+++ src/components/gameOption/GameOption.jsx
@@ -0,0 +1,13 @@
+import styles from '../gameOption/GameOption.module.css'
+
+
+function GameOption () {
+  return (
+    <div className={styles.gameOption}>
+      
+    </div>
+  )
+}
+
+
+export default GameOption
